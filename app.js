d70de44d8cb4c6ced3a028b20e06d4f2afafe75e
Andrey Leontev <leontev.andrey.s@gmail.com>
2017-11-24 17:55:29 +1000

    Add contact page and form

--- app.js
+++ app.js
@@ -20,5 +20,37 @@ app.get('/about', (req, res) => {
 	res.render('about');
 });
 
+app.get('/contact', (req, res) => {
+	res.render('contact');
+});
+
+app.post('/contact/send', (req, res) => {
+ const transporter = nodemailer.createTransport({
+  service: 'Gmail', 
+  auth: {
+   user: 'Enter email',
+   pass: 'Enter password'
+  }
+ });
+
+ const mailOptions = {
+  from: `Enter name <Enter email>`,
+  to: 'Enter email',
+  subject: 'Website Submission',
+  text: 'You have a submission with the following details... Name: '+req.body.name+'Email: '+req.body.email+ 'Message: '+req.body.message,
+  html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+ };
+
+ transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+   console.log(error);
+   res.redirect('/');
+  } else {
+   console.log('Message Sent: '+info.response);
+   res.redirect('/');
+  }
+ });
+});
+
 app.listen(3000);
 console.log('Server is runing on port 3000...');
